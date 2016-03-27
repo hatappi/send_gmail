8ba8f029ad5f4f29cf7e9de300a6d018e3109d53
h,yusaku <hata.yusaku.1225@gmail.com>
2016-03-27 21:04:55 +0900

    add list

--- lib/send_gmail.rb
+++ lib/send_gmail.rb
@@ -1,8 +1,16 @@
+require 'google/api_client'
 require "send_gmail/version"
 require 'send_gmail/auth'
 
 module SendGmail
   class Client
+    include SendGmail::Auth
 
+    attr_accessor :authorization, :client, :gmail_api
+
+    def initialize(app_name='gmail_test', app_ver='ver1.0')
+      @client = Google::APIClient.new(application_name: app_name, application_version: app_ver)
+      @gmail_api = client.discovered_api('gmail', 'v1')
+    end
   end
 end
--- lib/send_gmail/auth.rb
+++ lib/send_gmail/auth.rb
@@ -1,7 +1,28 @@
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/installed_app'
+require 'google/api_client/auth/storage'
+require 'google/api_client/auth/storages/file_store'
+require 'fileutils'
+
 module SendGmail
   module Auth
-    def test
-      'Fsafafsfs'
+    def authorize
+      credentials_path = '/tmp/client_secrets.json'
+      FileUtils.mkdir_p(File.dirname(credentials_path))
+
+      file_store = Google::APIClient::FileStore.new(credentials_path)
+      storage = Google::APIClient::Storage.new(file_store)
+      auth = storage.authorize
+
+      if auth.nil? || (auth.expired? && auth.refresh_token.nil?)
+        flow = Google::APIClient::InstalledAppFlow.new(
+          client_id: ENV['GOOGLE_CLIENT_ID'],
+          client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+          scope: SCOPE,
+        )
+        auth = flow.authorize(storage)
+      end
+      @client.authorization = auth
     end
   end
 end
